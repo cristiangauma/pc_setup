e99344a4d6dd9365f3338bbffc53e6b1b831eb20
Cristian Gauxachs Marin <cristian@gauxachs.com>
2022-09-11 21:55:20 +0200

    Update code

---
+++ install_dependences.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "Installing dependencies..."
+
+# Install dependencies
+sudo zypper install -y git make python3-pip
+python3 -m pip install --user ansible
+
+/home/cristian/.local/bin/ansible-galaxy install gantsign.oh-my-zsh
+/home/cristian/.local/bin/ansible-galaxy collection install community.general
+
+/home/cristian/.local/bin/ansible-playbook -i 127.0.0.1, -c local ansible/main.yml --ask-become-pass
